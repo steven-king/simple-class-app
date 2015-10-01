e6ce4e528686b0d3b378f46964dac909635a4a29
steven-king <cujophoto@gmail.com>
2015-10-01 00:37:35 -0400

    added Parse form

--- app/example/scripts/FormController.js
+++ app/example/scripts/FormController.js
@@ -1,36 +1,32 @@
 angular
   .module('example')
   .controller('FormController', function($scope) {
-    Parse.initialize("W9uXFiwaQ0ZNx4oG0JWC7rbWZHtA3w9bi1LxfqQd", "HZgOleLtk18UAtDCFyA4lMIZBtA0tQus2qXyzSC4");
 
+    $scope.formParse = function(form) {
+      supersonic.logger.info(form);
+      supersonic.logger.info($scope.name);
 
-    $scope.submit = function() {
-      supersonic.loggger.info("loaded function");
-          var user = new Parse.User();
-            user.set("username", $scope.username);
-            user.set("password", $scope.password);
-            user.set("email", $scope.email);
+      var user = new Parse.User();
+        user.set("username", $scope.name);
+        user.set("password", "my pass");
+        user.set("email", $scope.email);
 
-            // other fields can be set just like with Parse.Object
-            user.set("phone", "650-555-0001");
+        // other fields can be set just like with Parse.Object
+        user.set("phone", "650-555-0001");
 
-            supersonic.loggger.info(user);
-            user.signUp(null, {
-              success: function(user) {
-                // Hooray! Let them use the app now.
-                supersonic.loggger.info("user added");
-              },
-              error: function(user, error) {
-                // Show the error message somewhere and let the user try again.
-                supersonic.logger.error("Error: " + error.code + " " + error.message);
-              }
-    });
+        user.signUp(null, {
+          success: function(user) {
+            // Hooray! Let them use the app now.
+            supersonic.logger.info("user added");
+          },
+          error: function(user, error) {
+            // Show the error message somewhere and let the user try again.
+            supersonic.logger.error("Error: " + error.code + " " + error.message);
+          }
+        });
+
 
     }
-
-
-
-
 
     $scope.testDataConnection = function(){
       supersonic.logger.info("data connect started.");
